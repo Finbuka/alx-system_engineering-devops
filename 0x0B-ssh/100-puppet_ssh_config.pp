d8fa28a5dbd4ac7c71a44c990ed2a52cd4ca0936
Finbuka <ebukajoe100@gmail.com>
2022-10-17 04:30:18 +0100

    changed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,21 @@
-# sets up a client SSH configuration file so that we can connect to a server without using a password
+# Client configuration file (w/ Puppet)
+# Your SSH client configuration must be configured to use
+# the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse
+# to authenticate using a password
 include stdlib
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
-  replace => true,
-}
+file_line { 'NO password':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    PasswordAuthentication no',
+    match              => '^PasswordAuthentication',
+    append_on_no_match => true,
+  }
 
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
-}
+file_line { 'New private key holder':
+    ensure             => present,
+    path               => '/etc/ssh/ssh_config',
+    line               => '    IdentityFile ~/.ssh/school',
+    match              => '^IdentityFile',
+    append_on_no_match => true,
+  }
